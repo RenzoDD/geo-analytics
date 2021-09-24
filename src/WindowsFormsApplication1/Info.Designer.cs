cc590bf47ac54e43d53b2bfd27c5e164b535db2f
Renzo Diaz <renzodd2001@gmail.com>
2021-09-23 20:18:30 -0500

    Added Source

---
+++ src/WindowsFormsApplication1/Info.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Info
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Info));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Iskoola Pota", 48F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(60, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(307, 74);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "UniTeam";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Iskoola Pota", 21.75F);
+            this.label2.Location = new System.Drawing.Point(12, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 33);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Integrantes:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Iskoola Pota", 15.75F);
+            this.label3.Location = new System.Drawing.Point(28, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(240, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "·Renzo Mauricio Díaz Díaz";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Iskoola Pota", 15.75F);
+            this.label4.Location = new System.Drawing.Point(30, 145);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(202, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "·Andrés Mares Arévalo";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Iskoola Pota", 15.75F);
+            this.label5.Location = new System.Drawing.Point(28, 195);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(269, 24);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "·Adrian Gabriel Moscol Suarez";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Iskoola Pota", 15.75F);
+            this.label6.Location = new System.Drawing.Point(28, 170);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(223, 24);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "·Yeferson Carreño Cauna";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Iskoola Pota", 15.75F);
+            this.label7.Location = new System.Drawing.Point(28, 220);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(294, 24);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "·Jean-Patrick Yemsi Sanchez Rios";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Iskoola Pota", 15.75F);
+            this.label8.Location = new System.Drawing.Point(28, 317);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(381, 24);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "·Profesor: Antonio Marcos Medina Martinez";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Iskoola Pota", 21.75F);
+            this.label9.Location = new System.Drawing.Point(12, 283);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(270, 33);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "Información adicional:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Iskoola Pota", 15.75F);
+            this.label11.Location = new System.Drawing.Point(28, 342);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(250, 24);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "·Curso: Matemática Discreta";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Iskoola Pota", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(30, 367);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(145, 24);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "·Sección: SW31";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Iskoola Pota", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(96, 421);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(226, 43);
+            this.label12.TabIndex = 12;
+            this.label12.Text = "Ciclo 2019-1";
+            // 
+            // Info
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 492);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Info";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Info";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label12;
+    }
+}
